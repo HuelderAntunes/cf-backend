b4b8ff427bfb204c4b4682997511505c75556f11
Huelder <huelderantuness@gmail.com>
2020-01-20 22:08:10 -0300

    [ADD] added user tests

--- backend/login/tests.py
+++ backend/login/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from collections import OrderedDict
+class LoginTest(APITestCase):
+    client = APIClient()
+    user_data = {   "username": "Teste",
+                        "password": "teste321",
+                        "email": "teste@gmasi.com",
+                        "groups": []    }
 
-# Create your tests here.
+    def test_user(self):
+        response = self.client.post('/user/', self.user_data, format='json')
+        self.assertEqual(response.data, 
+                        self.user_data, "Be able to create user")
+
+        response = self.client.get('/user/1/', format='json')
+        self.assertEqual(response.data, self.user_data, 
+                        "Be able to get an user")
+
+        put_data = {"email": "teste@gmail.com"}
+        response = self.client.put('/user/1/', self.user_data, format='json')
+        self.assertEqual(response.data, self.user_data, 
+                        "Be able to edit an user")
