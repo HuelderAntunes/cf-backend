9280eadf7c484a2edc4bf70e12996cf8c6e6ca3d
Huelder <huelderantuness@gmail.com>
2020-01-20 22:20:38 -0300

    [ADD] added requirements.txt and remove count test

--- backend/login/tests.py
+++ backend/login/tests.py
@@ -13,6 +13,10 @@ class LoginTest(APITestCase):
         self.assertEqual(response.data, 
                         self.user_data, "Be able to create user")
 
+        """response = self.client.get('/user/', format='json')
+        self.assertEqual(response.data.count, 1, 
+                        "Be able to get all users")"""
+
         response = self.client.get('/user/1/', format='json')
         self.assertEqual(response.data, self.user_data, 
                         "Be able to get an user")
@@ -21,3 +25,5 @@ class LoginTest(APITestCase):
         response = self.client.put('/user/1/', self.user_data, format='json')
         self.assertEqual(response.data, self.user_data, 
                         "Be able to edit an user")
+
+        
